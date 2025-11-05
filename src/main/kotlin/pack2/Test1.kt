37e81605d4f42f9ce6cc783c01fd91b76ca69fd9
khlee <panpan68@naver.com>
2025-11-05 18:31:04 +0900

    pack fun, class, enum 기본 사용

--- src/main/kotlin/pack2/Test1.kt
+++ src/main/kotlin/pack2/Test1.kt
@@ -21,17 +21,6 @@ class Test1 {
     //본문 타입 생략 -> 타입 추론
     fun max2(a:Int, b:Int) = if(a>b) a else b
 
-    fun getMnemonic(color: Color) =
-        when(color){
-            Color.RED -> "Richard"
-            Color.ORANGE -> "Of"
-        }
-
-    fun getWarm(color: Color) =
-        when(color){
-            Color.RED, Color.ORANGE  -> "warm"
-        }
-
     fun mix(c1:Color, c2:Color) =
         when(setOf(c1, c2)){
             setOf(Color.RED) -> Color.ORANGE
